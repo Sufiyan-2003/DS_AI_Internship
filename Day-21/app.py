93d09d8a755849c1b461e00ff2a756f3c12dbc03
Sufiyan Ahmed S A <saheaven2141@gmail.com>
2026-02-25 14:23:04 +0530

    Add files via upload

---
+++ Day-21/app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+df = pd.read_csv("D:\VS-Studio\DS_AI_Internship\Day-21\Iris.csv")
+
+print("First 5 Rows:\n", df.head())
+print("\nDataset Info:\n")
+print(df.info())
+
+df = df.drop("Id", axis=1)
+
+X = df.drop("Species", axis=1)
+y = df["Species"]
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("\nAccuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
